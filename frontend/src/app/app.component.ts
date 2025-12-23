3d6e7832cf369118f1557e1813791ed1c21ec2a4
Георгий <drug200221@gmail.com>
2025-12-23 16:06:47 +0300

    0.x/dev - инициализировал проект клиентской части

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'psk-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  standalone: true,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+}
