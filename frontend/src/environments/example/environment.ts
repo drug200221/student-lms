ec4b71c1e43057a99957a4656253ea31359ef036
Георгий <drug200221@gmail.com>
2025-12-25 13:27:57 +0300

    0.x/dev - FRONT добавил базовый файл настроек окружения

---
+++ frontend/src/environments/example/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  WEBSOCKET_HOSTNAME: 'localhost',
+  WEBSOCKET_PORT: '3000',
+  ROOT_BACKEND_URL: 'http://localhost:8080/student',
+  BASE_API_ROUTE: '/lms/api/v1',
+};
